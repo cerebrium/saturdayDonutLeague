e23d59c41ea2f7bcf1986b4e873ffd4be424ad5c
cerebrium <nicholas.m.shankland@gmail.com>
2019-12-05 14:06:44 -0800

    google oauth is running, about to save data to local storage and to local database, setting up mongo

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { GoogleLogin } from 'react-google-login';
 import './App.css';
 
 function App() {
+  // Setting state with the user details retrieved using googles Oauth
+  const [userName, setUserName] = useState('');
+  const [userId, setUserId] = useState('');
+
+  const responseGoogle = (response) => {
+    setUserName(response.profileObj.givenName)
+    setUserName(response.profileObj.googleId)
+    console.log(response);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to Saturday Donut League</h1>
+      <h3>please login with your google account</h3>
+      <GoogleLogin
+        clientId="801108272625-cbbc8i5j8v8s423p95mkte842cdp7d32.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+      />
     </div>
   );
 }
---
+++ server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express')
+const mongoose = require('mongoose')
+
+// instatiate app
+const app = express()
+
+// middleware
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// Starting up the database 
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.once('open', () => {
+    console.log(`connected to mongodb on ${db.host}:${db.port}... `)
+});
+db.on('error', (err) => {
+    console.log(`Database Error: \n${err}`)
+})
+
+app.use('/user', require('./routes/auth'))
+
+app.listen(process.env.PORT, () => {
+    console.log(`server running on port: ${process.env.PORT}`)
+})
